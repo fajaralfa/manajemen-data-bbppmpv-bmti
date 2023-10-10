25e8d80c8018ca5f4328bc982029db60f4a2b8ce
miftahgamer <mifskancil@gmail.com>
2023-10-10 14:25:45 +0700

    menambahkan dropdown pada sidebar

--- resources/js/Pages/Sidebar.svelte
+++ resources/js/Pages/Sidebar.svelte
@@ -1,13 +1,38 @@
 <script context="module">
     export { default as layout } from '../Components/Layout.svelte'
+    let active = false
 </script>
 
 
 <div class="sidebar">
-    <a class="active" href="#home">Dashboard</a>
-    <a href="peserta diklat">Peserta Diklat</a>
-    <a href="peserta diklat">Peserta PKL</a>
-    <a href="peserta diklat">Inventaris</a>
+    <div class="dropdown">
+        <button>Dashboard</button>
+        <div class="dropdown-content">
+            <a href="#">Lihat Data</a>
+            <a href="#">Tambah Data</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button>Peserta Diklat</button>
+        <div class="dropdown-content">
+            <a href="#">Lihat Data</a>
+            <a href="#">Tambah Data</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button>Peserta PKL</button>
+        <div class="dropdown-content">
+            <a href="#">Lihat Data</a>
+            <a href="#">Tambah Data</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button>Inventaris</button>
+        <div class="dropdown-content">
+            <a href="#">Lihat Data</a>
+            <a href="#">Tambah Data</a>
+        </div>
+    </div>
 </div>
 
 <style>
@@ -15,11 +40,34 @@
         background-color:var(--bg-color);
         @apply m-0 p-0 w-52 fixed h-full overflow-auto;
     }
-    .sidebar a {
-        @apply block p-4 ;
+    .sidebar button {
+        @apply block p-4 w-full text-left;
     }
-    .sidebar a:hover{
+    .sidebar button:hover{
         background-color: rgba(255, 255, 255, 0.783);
         @apply text-black;
+    }
+    .dropdown{
+        position: relative;
+    }
+    .dropdown-content{
+        display: none;
+        position: relative;
+        background-color: rgba(164, 163, 163, 0.795);
+        width: 100%;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    } 
+    .dropdown-content a{
+        padding: 12px 16px;
+        text-decoration: none;
+        display:block;
+    }
+    .dropdown-content a:hover{
+        background-color: #f1f1f1;
+        color: black;
+    }
+    .dropdown:hover .dropdown-content{
+        display: block;
     }
 </style>
