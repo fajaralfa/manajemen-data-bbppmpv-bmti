1350709716fbbf5c3a0080ea61f80f7e11461c00
vutri123 <vutrianandamaudi@gmail.com>
2023-10-12 11:13:22 +0700

    overflow scroll tabel

--- resources/js/Pages/Table.svelte
+++ resources/js/Pages/Table.svelte
@@ -1,32 +1,35 @@
 <script context="module">
     export { default as layout } from '../Components/Layout.svelte'
     let data=[
-        {nama:'vutri',kelas:'12',jurusan:'rpl',alamat:'cipari',hobi:'renang'},
-        {nama:'miftah',kelas:'12',jurusan:'rpl',alamat:'petakan',hobi:'voly'},
-        {nama:'alfa',kelas:'12',jurusan:'rpl',alamat:'wangun',hobi:'ngoding'},
-        {nama:'holland',kelas:'12',jurusan:'rpl',alamat:'cipari',hobi:'akting'},
+        {nama:'vutri',kelas:'12',jurusan:'rpl',alamat:'cipari',hobi:'renang',pacar:'Holland',mantan:'zoro',sekolah:'stm',favwarna:'cream',
+        favmakanan:'nasgor,seblak',favminuman:'ollate',favfilm:'marvel'},
     ]
+    for(let i =0;i<100; i++){
+        data.push(data[0])
+    }
     console.log(data)
 </script>
-
-<table class="border-black border">
-    <tr>
-        {#each Object.entries(data[0]) as [key, value] }
-        <th>
-            {key}
-        </th>
-    {/each}
-    </tr>
-    {#each data as obj }
-        <tr >
-            {#each Object.entries(obj) as [key, value] }
-                <td class="px-10">
-                    {value}
-                </td>
-            {/each}
+<div class="overflow-scroll"> 
+    <table class="border-black border">
+        <tr>
+            {#each Object.entries(data[0]) as [key, value] }
+            <th>
+                {key}
+            </th>
+        {/each}
         </tr>
-    {/each}
-</table>
+        {#each data as obj }
+            <tr >
+                {#each Object.entries(obj) as [key, value] }
+                    <td class="px-10">
+                        {value}
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </table>
+</div>
+
 
 <style>
     table tr th{
@@ -40,5 +43,8 @@
     }
     tr:nth-child(even){
         background-color: rgba(200, 200, 200, 0.5);
+    }
+    tr:hover{
+        background-color: rgba(32, 213, 245, 0.5);
     }
 </style>
