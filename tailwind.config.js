7a7d4f1e971922a869d10c0a2b9761b72f8b08f0
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-10-13 14:40:44 +0700

    menambahkan dependensi daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./resources/**/*.{js,svelte,php}"],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [daisyui],
+    daisyui: {
+        themes: ['dark']
+    }
 };
