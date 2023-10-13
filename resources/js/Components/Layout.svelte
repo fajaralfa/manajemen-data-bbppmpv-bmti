88e56418abb5a73db534cbe51cd4cfa2b03e21aa
miftahgamer <mifskancil@gmail.com>
2023-10-13 15:57:48 +0700

    mengubah layout

--- resources/js/Components/Layout.svelte
+++ resources/js/Components/Layout.svelte
@@ -1,10 +1,60 @@
-<script>
-    import Topbar from './Topbar.svelte'
-    import Sidebar from '../Pages/Sidebar.svelte'
-</script>
+<div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+        <!-- Page content here -->
+        <div class="navbar bg-base-100">
+            <div class="flex-none">
+                <label for="my-drawer" class="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="inline-block w-5 h-5 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        ></path></svg
+                    >
+                </label>
+            </div>
+            <div class="flex-1">
+                <a class="btn btn-ghost normal-case text-xl">Aplikasi</a>
+            </div>
+            <div class="flex-none">
+                <button class="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="inline-block w-5 h-5 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                        ></path></svg
+                    >
+                </button>
+            </div>
+        </div>
+        <slot />
+    </div>
+    <div class="drawer-side">
+        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu py-16 px-0 w-80 min-h-full bg-base-200 text-base-content">
+            <!-- Sidebar content here -->
+            <li><a>Dashboard</a></li>
+            <li><a>Prakerin</a></li>
+            <li><a>Diklat</a></li>
+            <li><a>Inventaris</a></li>
+        </ul>
+    </div>
+</div>
 
-<main>
-    <Topbar />
-    <Sidebar />
-    <slot />
-</main>
+<style>
+    a {
+        @apply rounded-none;
+    }
+</style>
