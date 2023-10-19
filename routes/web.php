ab8fd7e0e4249e2bb455da0d81fd99ba7e05460b
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-10-19 20:42:18 +0700

    menambahkan middleware auth

--- routes/web.php
+++ routes/web.php
@@ -23,26 +23,29 @@ Route::post('/register', [UserController::class, 'register']);
 Route::inertia('/login', 'Login');
 Route::post('/login', [UserController::class, 'login']);
 
-Route::inertia('/home', 'Home')->name('home');
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::inertia('/home', 'Home')->name('home');
 
-//tampilan 
-Route::get('/diklat', [DiklatController::class, 'view']);
-Route::get('/prakerin', [PrakerinController::class, 'view']);
-Route::get('/inventaris', [InventarisController::class, 'view']);
-Route::inertia('/diklat/add', 'Diklat/Form');
-Route::inertia('/prakerin/add', 'Prakerin/Form');
-Route::inertia('/inventaris/add', 'Inventaris/Form');
-Route::get('/diklat/{id}/edit', [DiklatController::class, 'edit']);
-Route::get('/prakerin/{id}/edit', [PrakerinController::class, 'edit']);
-Route::get('/inventaris/{id}/edit', [InventarisController::class, 'edit']);
+    //tampilan
+    Route::get('/diklat', [DiklatController::class, 'view']);
+    Route::get('/prakerin', [PrakerinController::class, 'view']);
+    Route::get('/inventaris', [InventarisController::class, 'view']);
+    Route::inertia('/diklat/add', 'Diklat/Form');
+    Route::inertia('/prakerin/add', 'Prakerin/Form');
+    Route::inertia('/inventaris/add', 'Inventaris/Form');
+    Route::get('/diklat/{id}/edit', [DiklatController::class, 'edit']);
+    Route::get('/prakerin/{id}/edit', [PrakerinController::class, 'edit']);
+    Route::get('/inventaris/{id}/edit', [InventarisController::class, 'edit']);
 
-//aksi
-Route::post('/diklat', [DiklatController::class, 'store']);
-Route::post('/prakerin/{id}', [PrakerinController::class, 'store']);
-Route::post('/inventaris/{id}', [InventarisController::class, 'store']);
-Route::delete('/diklat/{id}', [DiklatController::class, 'delete']);
-Route::delete('/prakerin/{id}', [PrakerinController::class, 'delete']);
-Route::delete('/inventaris/{id}', [InventarisController::class, 'delete']);
-Route::put('/diklat/{id}', [DiklatController::class, 'update']);
-Route::put('/prakerin/{id}', [PrakerinController::class, 'update']);
-Route::put('/inventaris/{id}', [InventarisController::class, 'update']);
+    //aksi
+    Route::post('/diklat', [DiklatController::class, 'store']);
+    Route::post('/prakerin/{id}', [PrakerinController::class, 'store']);
+    Route::post('/inventaris/{id}', [InventarisController::class, 'store']);
+    Route::delete('/diklat/{id}', [DiklatController::class, 'delete']);
+    Route::delete('/prakerin/{id}', [PrakerinController::class, 'delete']);
+    Route::delete('/inventaris/{id}', [InventarisController::class, 'delete']);
+    Route::put('/diklat/{id}', [DiklatController::class, 'update']);
+    Route::put('/prakerin/{id}', [PrakerinController::class, 'update']);
+    Route::put('/inventaris/{id}', [InventarisController::class, 'update']);
+});
