fd4aba1e1068533911e1b0ae0767a34929458a5c
MfthRyd <mifskancil@gmail.com>
2023-10-20 13:47:57 +0700

    menambahkan link ubah dan hapus pada tabel

--- resources/js/Pages/Diklat/View.svelte
+++ resources/js/Pages/Diklat/View.svelte
@@ -2,10 +2,13 @@
     import Layout from '../../Components/Layout.svelte' 
     import Table from '../../Components/Table.svelte'
     
-    export let data 
-    console.log(data)
+    // export let data 
+    // console.log(data)
+    let data = [
+        {'ID':2, 'NAMA':'Miftah'}
+    ]
 </script>
 
 <Layout>
-    <Table {data}/>
+    <Table {data} urlGroup='diklat'/>
 </Layout>
