78b32b7da880018c4f1cc943bb040e5bbb6d2105
vutri123 <vutrianandamaudi@gmail.com>
2023-10-20 13:50:42 +0700

    input data foto gagal

--- resources/js/Pages/Diklat/View.svelte
+++ resources/js/Pages/Diklat/View.svelte
@@ -3,9 +3,11 @@
     import Table from '../../Components/Table.svelte'
     
     export let data 
+    export let message = null
     console.log(data)
 </script>
 
 <Layout>
+    <div class="bg-red-600">{message}</div>
     <Table {data}/>
 </Layout>
