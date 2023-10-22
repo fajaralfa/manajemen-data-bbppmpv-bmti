5404a4997edad7e6374a0ebecd2fae22c8bc8f24
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-10-22 17:03:46 +0700

    update table component

--- resources/js/Components/Table.svelte
+++ resources/js/Components/Table.svelte
@@ -5,27 +5,16 @@
 
     export let urlGroup
     export let data = [{}]
-
-    let editMode = false,
-        deleteMode = false
-
-    console.log(data)
 </script>
 
 <div class="flex px-3 py-1 bg-glass-dark gap-20">
     <div class="text-lg font-bold uppercase flex-grow"><slot>Judul Tabel</slot></div>
-    <div>
-        <input type="checkbox" bind:checked={deleteMode} id="" />
-        <input type="checkbox" bind:checked={editMode} id="" />
-    </div>
 </div>
 <div class="overflow-x-scroll table-container bg-glass-dark">
     <table class="table">
         <thead class=" text-white uppercase sticky top-0 bg-glass-dark">
             <tr class="border-b-4">
-                {#if deleteMode || editMode}
-                    <th colspan={deleteMode && editMode ? '2' : '1'}>Aksi</th>
-                {/if}
+                <th colspan="2">Aksi</th>
                 {#each Object.entries(data[0]) as [key]}
                     <th>{key}</th>
                 {/each}
@@ -34,21 +23,17 @@
         <tbody class="text-white">
             {#each data as row}
                 <tr>
-                    {#if deleteMode}
-                        <td>
-                            <a href="/{urlGroup}/{row['ID']}" use:inertia={{method: 'put'}} as='button'><DeleteIcon /></a>
-                        </td>
-                    {/if}
-                    {#if editMode}
-                        <td>
-                            <a href="/{urlGroup}/{row['ID']}/edit" use:inertia={{method: 'delete'}} as='button'><EditIcon /></a>
-                        </td>
-                    {/if}
+                    <td>
+                        <a href="/{urlGroup}/{row['ID']}" use:inertia={{ method: 'delete' }} as="button"><DeleteIcon /></a>
+                    </td>
+                    <td>
+                        <a href="/{urlGroup}/{row['ID']}/edit" use:inertia as="button"><EditIcon /></a>
+                    </td>
                     {#each Object.entries(row) as [key, val]}
                         {#if key !== 'FOTO'}
                             <td>{val}</td>
                         {:else}
-                            <td><img src="/diklat/photo/{row['FOTO']?.split('/')[1] ?? '0'}" alt="Pas Foto" srcset=""></td>
+                            <td><img src="/diklat/photo/{row['FOTO']?.split('/')[1] ?? '0'}" alt="Pas Foto" srcset="" /></td>
                         {/if}
                     {/each}
                 </tr>
