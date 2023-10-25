7c8f27a5bb3cf5f9a4ef57eccdbb658677bf11a2
vutri123 <vutrianandamaudi@gmail.com>
2023-10-25 10:10:22 +0700

    membuat desain tampilan  form import

--- resources/js/Pages/Diklat/FormImport.svelte
+++ resources/js/Pages/Diklat/FormImport.svelte
@@ -15,8 +15,18 @@
 </script>
 
 <Layout>
-    <form on:submit|preventDefault={importFile}>
-        <input type="file" on:input={(e) => input.file = e.target.files[0]}>
-        <button type="submit">Import</button>
-    </form>
+    <div class="h-[91vh] grid place-items-center">
+        <div class="card w-96 h-64 bg-glass-dark">
+            <div class="card-body">
+                <div class="text-center">Import data peserta diklat dan sekolah dari Spreadsheet.</div>
+                <div class="text-center"><a href="" class="text-blue-600 underline">Lihat Format</a></div>
+                <form class="mt-6" on:submit|preventDefault={importFile}>
+                    <div class="flex flex-col gap-y-4 items-center">
+                        <input type="file" on:input={(e) => input.file = e.target.files[0]}>
+                        <button type="submit" class="rounded bg-slate-800 px-2 py-1 text-xl uppercase">Import</button>
+                    </div>
+                </form>
+            </div>
+          </div>
+    </div>
 </Layout>
