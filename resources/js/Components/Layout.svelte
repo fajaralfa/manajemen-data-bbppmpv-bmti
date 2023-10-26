2b128be055a6f8723888608e81b0ab9d67af29e9
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-10-26 14:22:49 +0700

    extract svg icon from layout

--- resources/js/Components/Layout.svelte
+++ resources/js/Components/Layout.svelte
@@ -1,5 +1,7 @@
 <script>
     import { inertia } from '@inertiajs/svelte'
+    import ThreeDotIcon from '../Assets/ThreeDotIcon.svelte'
+    import HamburgerIcon from '../Assets/HamburgerIcon.svelte'
     let dropdownList = [false, false, false]
     function dropDownToggle(num) {
         if (dropdownList[num] === true) {
@@ -21,26 +23,13 @@
 
 <div class="navbar bg-black max-h-12 min-h-12 sticky top-0 z-[2]">
     <div class="flex-none">
-        <button class="btn btn-square btn-ghost" on:click={sidebarToggle}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"
-                ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg
-            >
-        </button>
+        <button class="btn btn-square btn-ghost" on:click={sidebarToggle}><HamburgerIcon /></button>
     </div>
     <div class="flex-1">
         <a class="btn btn-ghost normal-case text-xl">Aplikasi</a>
     </div>
     <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"
-                ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path></svg
-            >
-        </button>
+        <button class="btn btn-square btn-ghost"><ThreeDotIcon /></button>
     </div>
 </div>
 <div class="h-full fixed top-0 left-0 bg-glass-dark overflow-x-hidden {sidebarShow ? 'w-52' : 'w-0'}">
