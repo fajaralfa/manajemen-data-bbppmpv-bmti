88b53c2e7ce355fdb8c05999b191b379fc94bc0c
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-10-26 11:12:48 +0700

    memunculkan pesan data kosong jika begitu

--- resources/js/Components/Table.svelte
+++ resources/js/Components/Table.svelte
@@ -5,54 +5,65 @@
     import DeleteAlert from './DeleteAlert.svelte'
 
     export let urlGroup
-    export let data = [{}]
+    export let data = []
 
     console.log(data)
 
-    let showDeleteAlert = false
-    let deleteUrl = ''
-
-    function deleteConfirm(id) {
-        showDeleteAlert = true
-        deleteUrl = `/${urlGroup}/${id}`
+    let deleteAlertProps = {
+        showDeleteAlert: false,
+        deleteUrl: '',
     }
 
+    function deleteConfirm(id) {
+        deleteAlertProps.showDeleteAlert = true
+        deleteAlertProps.deleteUrl = `/${urlGroup}/${id}`
+    }
 </script>
 
-<DeleteAlert bind:deleteUrl bind:showDeleteAlert/>
+<DeleteAlert {...deleteAlertProps} />
 
 <div class="flex px-3 py-1 bg-glass-dark gap-20">
     <div class="text-lg font-bold uppercase flex-grow"><slot>Table</slot></div>
 </div>
-<div class="overflow-x-scroll table-container bg-glass-dark">
-    <table class="table table-xs">
-        <thead class=" text-white uppercase sticky top-0 bg-black">
-            <tr class="border-b-4">
-                {#each Object.entries(data[0]) as [key]}
-                    <th>{key}</th>
-                {/each}
-                <th class="right-0 sticky bg-black">Aksi</th>
-            </tr>
-        </thead>
-        <tbody class="text-white">
-            {#each data as row}
-                <tr>
-                    {#each Object.entries(row) as [key, val]}
-                        {#if key !== 'FOTO'}
-                            <td>{val}</td>
-                        {:else}
-                            <td><img src="/{urlGroup}/photo/{row['FOTO']?.split('/')[1] ?? '0'}" alt="Pas Foto" srcset="" /></td>
-                        {/if}
+{#if data.length == 0}
+    <div class="text-xl text-center">Data Kosong!</div>
+{:else}
+    <div class="overflow-x-scroll table-container bg-glass-dark">
+        <table class="table table-xs">
+            <thead class=" text-white uppercase sticky top-0 bg-black">
+                <tr class="border-b-4">
+                    {#each Object.entries(data[0]) as [key]}
+                        <th>{key}</th>
                     {/each}
-                    <td class="right-0 sticky bg-glass-dark">
-                        <button class="p-4" on:click={() => deleteConfirm(row['id'])}><DeleteIcon /></button>
-                        <button class="p-4" use:inertia={{'href': `/${urlGroup}/${row['id']}/edit`}} ><EditIcon /></button>
-                    </td>
+                    <th class="right-0 sticky bg-black">Aksi</th>
                 </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody class="text-white">
+                {#each data as row}
+                    <tr>
+                        {#each Object.entries(row) as [key, val]}
+                            {#if key !== 'FOTO'}
+                                <td>{val}</td>
+                            {:else}
+                                <td
+                                    ><img
+                                        src="/{urlGroup}/photo/{row['FOTO']?.split('/')[1] ?? '0'}"
+                                        alt="Pas Foto"
+                                        srcset=""
+                                    /></td
+                                >
+                            {/if}
+                        {/each}
+                        <td class="right-0 sticky bg-glass-dark">
+                            <button class="p-4" on:click={() => deleteConfirm(row['id'])}><DeleteIcon /></button>
+                            <button class="p-4" use:inertia={{ href: `/${urlGroup}/${row['id']}/edit` }}><EditIcon /></button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+{/if}
 
 <style>
     tr,
