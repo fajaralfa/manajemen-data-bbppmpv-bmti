aee33e4c30407495ff075444422f609292365166
MfthRyd <mifskancil@gmail.com>
2023-10-31 15:13:20 +0700

    membuat filter prakerin

--- app/Repository/PrakerinRepository.php
+++ app/Repository/PrakerinRepository.php
@@ -7,4 +7,15 @@ use Illuminate\Support\Facades\DB;
 class PrakerinRepository extends Repository
 {
     protected string $table = 'prakerin';
+
+    public function filterNamaNisTahun(?string $nama, ?string $nis, ?string $tahun, array $columns = ['*'])
+    {
+        $result = DB::table($this->table);
+
+        if (!empty($nama)) $result = $result->where('NAMA LENGKAP', 'LIKE', "%$nama%");
+        if (!empty($nis)) $result = $result->where('NIS/NIM', $nis);
+        if (!empty($tahun)) $result = $result->whereRaw('YEAR(`TANGGAL MASUK`) = ?', [$tahun]);
+
+        return $result->get($columns);
+    }
 }
--- resources/js/Pages/Prakerin/View.svelte
+++ resources/js/Pages/Prakerin/View.svelte
@@ -1,18 +1,41 @@
 <script>
+    import { router } from '@inertiajs/svelte'
     import Layout from '../../Components/Layout.svelte'
     import Table from '../../Components/Table.svelte'
 
     export let data
-    export let message
     console.log(data)
-    console.log(message)
+
+    let query = {
+        nama: null,
+        nis: null,
+        tahun: null,
+    }
+
+    function find() {
+        router.get('/prakerin', query)
+    }
 </script>
 
 <Layout>
     <Table {data} urlGroup="prakerin">
         <div class="text-xl font-bold uppercase">Data Peserta Prakerin</div>
         <div>
-            <form>save</form>
+            <form on:submit|preventDefault={find}>
+                <div class="flex gap-x-4">
+                    <div>Filter:</div>
+                    <div>
+                        <input type="text" name="nama" placeholder="Nama" bind:value={query.nama} />
+                    </div>
+                    <div>
+                        <input type="text" name="nis" placeholder="NIS/NIM" bind:value={query.nis} />
+                    </div>
+                    <div>
+                        <input type="text" name="tahun" placeholder="Tahun Masuk" bind:value={query.tahun} />
+                    </div>
+                    <div><button type="submit" class="bg-slate-700 rounded px-2 py-[0.1rem] text-sm">Cari</button></div>
+                </div>
+            </form>
         </div>
     </Table>
 </Layout>
