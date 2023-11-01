e118dbc27bdf23e35d64e5555187c0641df99171
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-01 14:58:05 +0700

    membuat komponen input text dan textarea

---
+++ resources/js/Components/Formulir/InputText.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let value = ''
+    export let error = ''
+    $: console.log(error)
+</script>
+
+<div class="flex justify-between items-center">
+    <label for="nama"><slot>Field</slot></label>
+    <div class="max-w-xs w-full">
+        {#if error}
+            <div class="text-xs text-center">{error}</div>
+        {/if}
+        <input type="text" class="input w-full max-w-xs border {error ? 'border border-red-600' : ''}" bind:value />
+    </div>
+</div>
+
+<style>
+</style>
