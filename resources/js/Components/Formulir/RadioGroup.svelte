eeed2e036c5fd38b183a4a12f2ada8c86ef96b64
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-02 11:23:36 +0700

    membuat komponen option dan radio

--- resources/js/Components/Formulir/RadioGroup.svelte
+++ resources/js/Components/Formulir/RadioGroup.svelte
@@ -3,14 +3,22 @@
     export let options = {
         // label : value,
     }
+    export let error
 </script>
 
 <div class="flex justify-between items-center">
     <label for="jenis_kelamin"><slot>Radio Group Field</slot></label>
-    <div class="flex gap-3">
-        {#each Object.entries(options) as [label, val]}
-            <input type="radio" value={val} class="radio" bind:group={value} />
-            <label for="jenis_kelamin" value={val}>{label}</label>
-        {/each}
+    <div class="flex gap-3 flex-col">
+        {#if error}
+            <div class="text-xs text-center">{error}</div>
+        {/if}
+        <div class="p-2 flex flex-col gap-2 {error ? 'border border-red-600' : ''}">
+            {#each Object.entries(options) as [label, val]}
+                <label value={val} class="flex items-center gap-3">
+                    <input type="radio" value={val} class="radio" bind:group={value} />
+                    {label}
+                </label>
+            {/each}
+        </div>
     </div>
 </div>
