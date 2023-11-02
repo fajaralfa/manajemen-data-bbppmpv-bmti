454522580ff99c690acaba92654150ed40761171
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-02 13:47:26 +0700

    refactor form inventaris

--- resources/js/Components/Inventaris/Form.svelte
+++ resources/js/Components/Inventaris/Form.svelte
@@ -1,4 +1,10 @@
 <script>
+    import InputText from '../Formulir/InputText.svelte'
+    import InputFile from '../Formulir/InputFile.svelte'
+    import InputDate from '../Formulir/InputDate.svelte'
+    import TextArea from '../Formulir/TextArea.svelte'
+    import Option from '../Formulir/Option.svelte'
+
     export let input = {
         Kategori: null,
         Nama_Peralatan: null,
@@ -8,7 +14,7 @@
         Spesifikasi: null,
         Nomor_Seri: null,
         Satuan: null,
-        Volume: null,
+        Harga_Satuan: null,
         Harga_Satuan: null,
         Jumlah: null,
         Keterangan_Produk: null,
@@ -18,79 +24,25 @@
     }
 
     export let submit
+    export let errors
 </script>
 
 <div class="p-10">
     <form on:submit|preventDefault={() => submit(input)} class="flex flex-col gap-y-6 items-center">
         <div class="input-container">
-            <div>
-                <label for="Kategori">Kategori:</label>
-                <input type="text" name="kategori" bind:value={input.Kategori} class="input w-full max-w-xs" />
-            </div>
-            <div>
-                <label for="nama_barang">Nama Peralatan:</label>
-                <input type="text" name="nama_barang" bind:value={input.Nama_Peralatan} class="input w-full max-w-xs" />
-            </div>
-            <div>
-                <label for="foto">Foto:</label>
-                <input
-                    type="file"
-                    name="foto"
-                    on:input={(e) => (input.Gambar = e.target.files[0])}
-                    class="file-input file-input-bordered w-full max-w-xs"
-                />
-            </div>
-            <div>
-                <label for="merk">Merk:</label>
-                <input type="text" name="merk" bind:value={input.Merk} class="input w-full max-w-xs" />
-            </div>
-            <div>
-                <label for="tipe">Tipe:</label>
-                <input type="text" name="tipe" bind:value={input.Tipe} class="input w-full max-w-xs" />
-            </div>
-            <div>
-                <label for="spesifikasi">Spesifikasi:</label>
-                <textarea class="textarea" name="spesifikasi" bind:value={input.Spesifikasi}></textarea>
-            </div>
-            <div>
-                <label for="nomor_seri">Nomor Seri:</label>
-                <input type="text" name="nomor_seri" bind:value={input.Nomor_Seri} class="input w-full max-w-xs" />
-            </div>
-            <div>
-                <label for="satuan">Satuan:</label>
-                <select name="satuan" bind:value={input.Satuan} class="select select-bordered w-full max-w-xs">
-                    <option value="unit">Unit</option>
-                    <option value="slop">Slop</option>
-                </select>
-            </div>
-            <div>
-                <label for="volume">Volume:</label>
-                <input type="text" name="volume" class="input w-full max-w-xs" bind:value={input.Volume} />
-            </div>
-            <div>
-                <label for="harga_satuan">Harga Satuan:</label>
-                <input type="text" name="harga_satuan" class="input w-full max-w-xs" bind:value={input.Harga_Satuan} />
-            </div>
-            <div>
-                <label for="jumlah">Jumlah:</label>
-                <input type="text" name="jumlah" class="input w-full max-w-xs" bind:value={input.Jumlah} />
-            </div>
-            <div>
-                <label for="keterangan_produk">Keterangan Porduk:</label>
-                <textarea class="textarea" name="keterangan_produk" bind:value={input.Keterangan_Produk}></textarea>
-            </div>
-            <div>
-                <label for="link_produk">Link Produk:</label>
-                <input type="text" name="link_produk" class="input w-full max-w-xs" bind:value={input.Link_Produk} />
-            </div>
-            <div>
-                <label for="urgensi">Urgensi:</label>
-                <textarea class="textarea" name="urgensi" bind:value={input.Urgensi}></textarea>
-            </div>
-            <div>
-                <label for="waktu_pengadaan">Waktu Pengadaan:</label>
-                <input type="date" name="waktu_pengadaan" class="input w-full max-w-xs" bind:value={input.Waktu_Pengadaan} />
-            </div>
+            <InputText bind:value={input.Nama_Peralatan} error={errors.Nama_Peralatan}>Nama Peralatan</InputText>
+            <InputFile bind:value={input.Gambar} error={errors.Gambar}>Gambar</InputFile>
+            <InputText bind:value={input.Merk} error={errors.Merk}>Merk</InputText>
+            <InputText bind:value={input.Tipe} error={errors.Tipe}>Tipe</InputText>
+            <InputText bind:value={input.Spesifikasi} error={errors.Spesifikasi}>Spesifikasi</InputText>
+            <InputText bind:value={input.Nomor_Seri} error={errors.Nomor_Seri}>Nomor_Seri</InputText>
+            <Option bind:value={input.Satuan} options={['unit', 'slop']}>Satuan</Option>
+            <InputText bind:value={input.Harga_Satuan} error={errors.Harga_Satuan}>Harga Satuan</InputText>
+            <InputText bind:value={input.Harga_Satuan} error={errors.Harga_Satuan}>Harga Satuan</InputText>
+            <TextArea bind:value={input.Keterangan_Produk} error={errors.Keterangan_Produk}>Keterangan Produk</TextArea>
+            <InputText bind:value={input.Link_Produk} error={errors.Link_Produk}>Link Produk</InputText>
+            <InputText bind:value={input.Urgensi} error={errors.Urgensi}>Urgensi</InputText>
+            <InputDate bind:value={input.Waktu_Pengadaan} error={errors.Waktu_Pengadaan}>Waktu Pengadaan</InputDate>
             <div>
                 <input class="btn btn-neutral" type="submit" name="submit" value="Simpan" />
             </div>
--- resources/js/Pages/Inventaris/Form.svelte
+++ resources/js/Pages/Inventaris/Form.svelte
@@ -2,11 +2,14 @@
     import Form from '../../Components/Inventaris/Form.svelte'
     import Layout from '../../Components/Layout.svelte'
     import { router, page } from '@inertiajs/svelte'
+
     function submit(input) {
         router.post('/inventaris', input)
     }
+
+    export let errors = {}
 </script>
 
 <Layout>
-    <Form {submit} />
+    <Form {submit} {errors} />
 </Layout>
