9869f0cba6d4590453846477e724dfedce51f82c
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-02 12:01:04 +0700

    refactor form prakerin

--- resources/js/Components/Prakerin/Form.svelte
+++ resources/js/Components/Prakerin/Form.svelte
@@ -1,4 +1,9 @@
 <script>
+    import InputText from '../Formulir/InputText.svelte'
+    import InputFile from '../Formulir/InputFile.svelte'
+    import InputDate from '../Formulir/InputDate.svelte'
+    import RadioGroup from '../Formulir/RadioGroup.svelte'
+
     export let input = {
         NAMA_LENGKAP: null,
         'NIS/NIM': null,
@@ -26,159 +31,56 @@
     }
 
     export let submit
+    export let errors
 </script>
 
 <div class="p-10">
     <form on:submit|preventDefault={() => submit(input)} class="flex flex-col gap-y-6 items-center">
         <div class="input-container">
             <h1 class="uppercase font-bold text-xl mb-3">Identitas Siswa</h1>
-            <div>
-                <label for="nama">Nama Lengkap:</label>
-                <input type="text" name="nama" class="input w-full max-w-xs" bind:value={input.NAMA_LENGKAP} />
-            </div>
-            <div>
-                <label for="nis">NIS/NIM:</label>
-                <input type="text" name="nis" class="input w-full max-w-xs" bind:value={input['NIS/NIM']} />
-            </div>
-            <div>
-                <label for="bidang">Bidang Keahlian:</label>
-                <input type="text" name="bidang" class="input w-full max-w-xs" bind:value={input.BIDANG_KEAHLIAN} />
-            </div>
-            <div>
-                <label for="program">Program Keahlian:</label>
-                <input type="text" name="program" class="input w-full max-w-xs" bind:value={input.PROGRAM_KEAHLIAN} />
-            </div>
-            <div>
-                <label for="kompetensi_keahlian">Kompetensi Keahlian:</label>
-                <input
-                    type="text"
-                    name="kompetensi_keahlian"
-                    class="input w-full max-w-xs"
-                    bind:value={input.KOMPETENSI_KEAHLIAN}
-                />
-            </div>
-            <div>
-                <label for="tempat_lahir">Tempat Lahir:</label>
-                <input type="text" name="tempat_lahir" class="input w-full max-w-xs" bind:value={input.TEMPAT_LAHIR} />
-            </div>
-            <div>
-                <label for="tanggal_lahir">Tanggal Lahir:</label>
-                <input type="date" name="tanggal_lahir" class="input w-full max-w-xs" bind:value={input.TANGGAL_LAHIR} />
-            </div>
-            <div>
-                <label for="jenis_kelamin">Jenis kelamin:</label>
-                <label for="jenis_kelamin"
-                    ><input
-                        type="radio"
-                        name="jenis_kelamin"
-                        value="L"
-                        class="radio"
-                        bind:group={input.JENIS_KELAMIN}
-                    />Laki-laki</label
-                >
-                <label for="jenis_kelamin"
-                    ><input
-                        type="radio"
-                        name="jenis_kelamin"
-                        value="P"
-                        class="radio"
-                        bind:group={input.JENIS_KELAMIN}
-                    />Perempuan</label
-                >
-            </div>
-            <div>
-                <label for="agama">Agama:</label>
-                <input type="text" name="agama" class="input w-full max-w-xs" bind:value={input.AGAMA} />
-            </div>
-            <div>
-                <label for="alamat">Alamat Lengkap:</label>
-                <textarea class="textarea" name="alamat" bind:value={input.ALAMAT_LENGKAP}></textarea>
-            </div>
-            <div>
-                <label for="no_hp">No HP:</label>
-                <input type="text" name="no_hp" class="input w-full max-w-xs" bind:value={input.NO_HP} />
-            </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" name="email" class="input w-full max-w-xs" bind:value={input.EMAIL} />
-            </div>
-            <div>
-                <label for="hobby">Hobby:</label>
-                <input type="text" name="hobby" class="input w-full max-w-xs" bind:value={input.HOBBY} />
-            </div>
-            <div>
-                <label for="foto">Foto:</label>
-                <input
-                    type="file"
-                    name="foto"
-                    class="file-input file-input-bordered w-full max-w-xs"
-                    on:input={(e) => (input.FOTO = e.target.files[0])}
-                />
-            </div>
-            <div>
-                <label for="tanggal_masuk">Tanggal Masuk:</label>
-                <input type="date" name="tanggal_masuk" class="input w-full max-w-xs" bind:value={input.TANGGAL_MASUK} />
-            </div>
-            <div>
-                <label for="tanggal_keluar">Tanggal Keluar:</label>
-                <input type="date" name="tanggal_keluar" class="input w-full max-w-xs" bind:value={input.TANGGAL_KELUAR} />
-            </div>
-            <div>
-                <label for="tempat_departemen_pelaksanaan">Tempat Departemen Pelaksanaan:</label>
-                <input
-                    type="text"
-                    name="tempat_departemen_pelaksanaan"
-                    class="input w-full max-w-xs"
-                    bind:value={input['TEMPAT/DEPARTEMEN_PELAKSANAAN']}
-                />
-            </div>
+
+            <InputText bind:value={input.NAMA_LENGKAP} error={errors.NAMA_LENGKAP}>Nama Lengkap</InputText>
+            <InputText bind:value={input['NIS/NIM']} error={errors['NIS/NIM']}>NIS/NIM</InputText>
+            <InputText bind:value={input['BIDANG_KEAHLIAN']} error={errors['BIDANG_KEAHLIAN']}>Bidang Keahlian:</InputText>
+            <InputText bind:value={input['PROGRAM_KEAHLIAN']} error={errors['PROGRAM_KEAHLIAN']}>Program Keahlian</InputText>
+            <InputText bind:value={input['KOMPETENSI_KEAHLIAN']} error={errors['KOMPETENSI_KEAHLIAN']}>
+                Kompetensi Keahlian
+            </InputText>
+            <InputText bind:value={input['TEMPAT_LAHIR']} error={errors['TEMPAT_LAHIR']}>Kompetensi Keahlian</InputText>
+            <InputText bind:value={input['TANGGAL_LAHIR']} error={errors['TANGGAL_LAHIR']}>Kompetensi Keahlian</InputText>
+            <RadioGroup
+                bind:value={input['JENIS_KELAMIN']}
+                options={{ 'Laki - Laki': 'L', Perempuan: 'P' }}
+                error={errors['JENIS_KELAMIN']}>Jenis Kelamin</RadioGroup
+            >
+            <InputText bind:value={input['AGAMA']} error={errors['AGAMA']}>Agama</InputText>
+            <InputText bind:value={input['ALAMAT_LENGKAP']} error={errors['ALAMAT_LENGKAP']}>Kompetensi Keahlian</InputText>
+            <InputText bind:value={input['NO_HP']} error={errors['NO_HP']}>Kompetensi Keahlian</InputText>
+            <InputText bind:value={input['EMAIL']} error={errors['EMAIL']}>Kompetensi Keahlian</InputText>
+            <InputText bind:value={input['HOBBY']} error={errors['HOBBY']}>Hobi</InputText>
+            <InputFile bind:value={input['FOTO']} error={errors['FOTO']}></InputFile>
+            <InputDate bind:value={input['TANGGAL_MASUK']} error={errors['TANGGAL_MASUK']}>Tanggal Masuk</InputDate>
+            <InputDate bind:value={input['TANGGAL_KELUAR']} error={errors['TANGGAL_KELUAR']}>Tanggal Keluar</InputDate>
+            <InputText bind:value={input['TEMPAT/DEPARTEMEN_PELAKSANAAN']} error={errors['TEMPAT/DEPARTEMEN_PELAKSANAAN']}>
+                Tempat / Departemen Pelaksanaan
+            </InputText>
             <h1 class="uppercase font-bold text-xl mb-3 mt-5">Identitas Sekolah</h1>
-            <div>
-                <label for="nama_sekolah">Nama Sekolah:</label>
-                <input type="text" name="nama_sekolah" class="input w-full max-w-xs" bind:value={input.NAMA_SEKOLAH} />
-            </div>
-            <div>
-                <label for="kabupaten_kota_sekolah">Kabupaten/Kota Sekolah:</label>
-                <input
-                    type="text"
-                    name="kabupaten_kota_sekolah"
-                    class="input w-full max-w-xs"
-                    bind:value={input['KABUPATEN/KOTA_SEKOLAH']}
-                />
-            </div>
-            <div>
-                <label for="status_sekolah">Status Sekolah:</label>
-                <label for="status_sekolah"
-                    ><input
-                        type="radio"
-                        name="status_sekolah"
-                        value="Negeri"
-                        class="radio"
-                        bind:group={input.STATUS_SEKOLAH}
-                    />Negeri</label
-                >
-                <label for="status_sekolah"
-                    ><input
-                        type="radio"
-                        name="status_sekolah"
-                        value="Swasta"
-                        class="radio"
-                        bind:group={input.STATUS_SEKOLAH}
-                    />Swasta</label
-                >
-            </div>
-            <div>
-                <label for="nss">NSS:</label>
-                <input type="text" name="nss" class="input w-full max-w-xs" bind:value={input.NSS} />
-            </div>
-            <div>
-                <label for="alamat_sekolah">Alamat Lengkap Sekolah:</label>
-                <textarea class="textarea" name="alamat_sekolah" bind:value={input.ALAMAT_LENGKAP_SEKOLAH}></textarea>
-            </div>
-            <div>
-                <label for="posel_sekolah">Email Sekolah:</label>
-                <input type="email" name="posel_sekolah" class="input w-full max-w-xs" bind:value={input.POSEL_SEKOLAH} />
-            </div>
+            <InputText bind:value={input['NAMA_SEKOLAH']} error={errors['NAMA_SEKOLAH']}>Nama Sekolah</InputText>
+            <InputText bind:value={input['KABUPATEN/KOTA_SEKOLAH']} error={errors['KABUPATEN/KOTA_SEKOLAH']}>
+                Kabupaten Sekolah
+            </InputText>
+            <RadioGroup
+                bind:value={input['STATUS_SEKOLAH']}
+                options={{ Negeri: 'Negeri', Swasta: 'Swasta' }}
+                error={errors['STATUS_SEKOLAH']}
+            >
+                Status Sekolah
+            </RadioGroup>
+            <InputText bind:value={input['NSS']} error={errors['NSS']}>NSS</InputText>
+            <InputText bind:value={input['ALAMAT_LENGKAP_SEKOLAH']} error={errors['ALAMAT_LENGKAP_SEKOLAH']}>
+                Alamat Lengkap Sekolah
+            </InputText>
+            <InputText bind:value={input['POSEL_SEKOLAH']} error={errors['POSEL_SEKOLAH']}>Email Sekolah</InputText>
             <div>
                 <input class="btn btn-neutral" type="submit" name="submit" value="Simpan" />
             </div>
--- resources/js/Pages/Prakerin/Form.svelte
+++ resources/js/Pages/Prakerin/Form.svelte
@@ -2,18 +2,17 @@
     import { router, page } from '@inertiajs/svelte'
     import Layout from '../../Components/Layout.svelte'
     import Form from '../../Components/Prakerin/Form.svelte'
+
+    export let errors = {}
+
     function submit(input) {
         router.post('/prakerin', input)
     }
-
-    export let errors
-    console.log(errors)
-    $: console.log($page.props.flash)
 </script>
 
 <svelte:head>
     <title>Form Pendaftaran Peserta Prakerin</title>
 </svelte:head>
 <Layout>
-    <Form {submit} />
+    <Form {submit} {errors} />
 </Layout>
