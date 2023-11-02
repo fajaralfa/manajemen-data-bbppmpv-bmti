fb3b3b2e0174725ff725b7ee99410b8a8bce1f8f
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-02 14:30:15 +0700

    update default value component

--- resources/js/Components/Formulir/InputText.svelte
+++ resources/js/Components/Formulir/InputText.svelte
@@ -1,7 +1,7 @@
 <script>
     import Input from './Input.svelte'
-    export let value = ''
+    export let value
     export let error = ''
 </script>
 
-<Input type="text" binder={(e) => (value = e.currentTarget.value)} {error}><slot>Input Date</slot></Input>
+<Input type="text" {value} binder={(e) => (value = e.currentTarget.value)} {error}><slot>Input Date</slot></Input>
