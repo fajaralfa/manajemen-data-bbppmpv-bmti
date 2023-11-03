2dc19edb4b416bf194e71de46517a1592637541e
MfthRyd <mifskancil@gmail.com>
2023-11-03 10:08:46 +0700

    membuat indikator halaman di link navbar

--- resources/js/Components/Layout.svelte
+++ resources/js/Components/Layout.svelte
@@ -21,7 +21,8 @@
         sidebarShow = !sidebarShow
     }
 
-    let link = window.location.pathname.split('/')[1]
+    let link = window.location.pathname
+    $: console.log(link)
 </script>
 
 <div class="navbar bg-black max-h-12 min-h-12 sticky top-0 z-[2]">
@@ -46,42 +47,53 @@
     <ul class="navlist menu py-12 px-0 w-52 min-h-full bg-black text-base-content">
         <!-- Sidebar content here -->
         <li>
-            <a href="/home" use:inertia class={link == 'home' ? 'bg-slate-700' : ''}>Dashboard</a>
+            <a href="/home" use:inertia class={link.split('/')[1] == 'home' ? 'bg-slate-700' : ''}>Dashboard</a>
         </li>
         <li>
-            <a on:click={() => dropDownToggle(0)}>Prakerin</a>
+            <a class={link.split('/')[1] == 'prakerin' ? 'bg-slate-700' : ''} on:click={() => dropDownToggle(0)}>Prakerin</a>
             {#if dropdownList[0]}
                 <ul transition:slide>
-                    <li><a href="/prakerin/add" use:inertia>Tambah Data</a></li>
-                    <li><a href="/prakerin" use:inertia>Lihat Data</a></li>
+                    <li>
+                        <a href="/prakerin/add" use:inertia class={link == '/prakerin/add' ? 'bg-slate-700' : ''}>Tambah Data</a>
+                    </li>
+                    <li><a href="/prakerin" use:inertia class={link == '/prakerin' ? 'bg-slate-700' : ''}>Lihat Data</a></li>
                 </ul>
             {/if}
         </li>
         <li>
-            <a on:click={() => dropDownToggle(1)}>Diklat</a>
+            <a class={link.split('/')[1] == 'diklat' ? 'bg-slate-700' : ''} on:click={() => dropDownToggle(1)}>Diklat</a>
             {#if dropdownList[1]}
                 <ul transition:slide>
-                    <li><a href="/diklat/add" use:inertia>Tambah Data</a></li>
-                    <li><a href="/diklat" use:inertia>Lihat Data</a></li>
-                    <li><a href="/diklat/import" use:inertia>Import Data</a></li>
+                    <li><a href="/diklat/add" use:inertia class={link == '/diklat/add' ? 'bg-slate-700' : ''}>Tambah Data</a></li>
+                    <li><a href="/diklat" use:inertia class={link == '/diklat' ? 'bg-slate-700' : ''}>Lihat Data</a></li>
+                    <li>
+                        <a href="/diklat/import" use:inertia class={link == '/diklat/import' ? 'bg-slate-700' : ''}>Import Data</a
+                        >
+                    </li>
                 </ul>
             {/if}
         </li>
         <li>
-            <a on:click={() => dropDownToggle(2)}>Inventaris</a>
+            <a class={link.split('/')[1] == 'inventaris' ? 'bg-slate-700' : ''} on:click={() => dropDownToggle(2)}>Inventaris</a>
             {#if dropdownList[2]}
                 <ul transition:slide>
-                    <li><a href="/inventaris/add" use:inertia>Tambah Data</a></li>
-                    <li><a href="/inventaris" use:inertia>Lihat Data</a></li>
+                    <li>
+                        <a href="/inventaris/add" use:inertia class={link == '/inventaris/add' ? 'bg-slate-700' : ''}
+                            >Tambah Data</a
+                        >
+                    </li>
+                    <li><a href="/inventaris" use:inertia class={link == '/inventaris' ? 'bg-slate-700' : ''}>Lihat Data</a></li>
                 </ul>
             {/if}
         </li>
         <li>
-            <a on:click={() => dropDownToggle(3)}>Sekolah</a>
+            <a class={link.split('/')[1] == 'sekolah' ? 'bg-slate-700' : ''} on:click={() => dropDownToggle(3)}>Sekolah</a>
             {#if dropdownList[3]}
                 <ul>
-                    <li><a href="/sekolah/add" use:inertia>Tambah Data</a></li>
-                    <li><a href="/sekolah" use:inertia>Lihat Data</a></li>
+                    <li>
+                        <a href="/sekolah/add" use:inertia class={link == '/sekolah/add' ? 'bg-slate-700' : ''}>Tambah Data</a>
+                    </li>
+                    <li><a href="/sekolah" use:inertia class={link == '/sekolah' ? 'bg-slate-700' : ''}>Lihat Data</a></li>
                 </ul>
             {/if}
         </li>
