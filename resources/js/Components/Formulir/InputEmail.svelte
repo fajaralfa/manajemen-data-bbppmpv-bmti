fc41fc2caa53f96e93d449819e2630d1305a3666
vutri123 <vutrianandamaudi@gmail.com>
2023-11-06 13:55:25 +0700

    menambah komponen formulir

---
+++ resources/js/Components/Formulir/InputEmail.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Input from './Input.svelte'
+    export let value = ''
+    export let error = ''
+</script>
+
+<Input type="email" {value} binder={(e) => (value = e.currentTarget.value)} {error}><slot>Input Email</slot></Input>
