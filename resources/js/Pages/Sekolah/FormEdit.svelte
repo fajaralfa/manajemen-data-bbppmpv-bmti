87d4063285d97bc8e373cfe87072c755d357282d
MfthRyd <mifskancil@gmail.com>
2023-11-08 11:00:07 +0700

    fox form edit sekolah

--- resources/js/Pages/Sekolah/FormEdit.svelte
+++ resources/js/Pages/Sekolah/FormEdit.svelte
@@ -3,16 +3,18 @@
     import Layout from '../../Components/Layout.svelte'
     import Form from '../../Components/Sekolah/Form.svelte'
     export let input
+    export let errors = {}
 
     let idSekolah = window.location.pathname.split('/')[2]
     function submit(input) {
         router.post(`/sekolah/${idSekolah}/edit`, input)
     }
+    $: console.log(input)
 </script>
 
 <svelte:head>
     <title>Form Edit Data Sekolah</title>
 </svelte:head>
 <Layout>
-    <Form {submit} {input} />
+    <Form {submit} {input} {errors} />
 </Layout>
