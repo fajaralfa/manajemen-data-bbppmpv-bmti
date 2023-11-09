ccf27e763913afaaeed03317fa722325fa1c3324
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-09 09:44:37 +0700

    svelte + inertia code splitting

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,8 @@ import { createInertiaApp } from '@inertiajs/svelte'
 
 createInertiaApp({
     resolve: (name) => {
-        const pages =
-            import.meta.glob('./Pages/**/*.svelte', { eager: true })
-        return pages[`./Pages/${name}.svelte`]
+        const pages = import.meta.glob('./Pages/**/*.svelte')
+        return pages[`./Pages/${name}.svelte`]()
     },
     setup({ el, App, props }) {
         new App({ target: el, props })
