7f6524be1c50d25e4e43f5e9f90038a1bd8bc45e
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-21 11:14:12 +0700

    menambahkan bahasa inggris converter tanggal

--- app/Helper/Converter.php
+++ app/Helper/Converter.php
@@ -19,6 +19,19 @@ class Converter
             'oktober' => '10',
             'november' => '11',
             'desember' => '12',
+
+            'january' => '01',
+            'february' => '02',
+            'march' => '03',
+            'april' => '04',
+            'may' => '05',
+            'june' => '06',
+            'july' => '07',
+            'august' => '08',
+            'september' => '09',
+            'october' => '10',
+            'november' => '11',
+            'desember' => '12',
         ];
 
         $date = trim($date);
