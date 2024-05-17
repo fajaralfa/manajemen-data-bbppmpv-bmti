8a2f3dfb19e030fc5a8e99190328f490da858b92
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-05-17 14:01:39 +0700

    update npm packages [daisyui 3 -> 4]

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            '@css': path.resolve(__dirname, './resources/css')
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
