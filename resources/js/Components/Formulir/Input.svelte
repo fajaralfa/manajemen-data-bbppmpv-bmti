853b6878984e5b2d0c75c63e10dc1a5625aff00c
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-05-22 15:00:18 +0700

    add $$props.class to input

--- resources/js/Components/Formulir/Input.svelte
+++ resources/js/Components/Formulir/Input.svelte
@@ -11,6 +11,6 @@
         {#if error}
             <div class="text-xs text-center">{error}</div>
         {/if}
-        <input {type} {value} class="input w-full max-w-xs border {error ? 'border border-red-600' : ''}" on:input={binder} />
+        <input {type} {value} class="input w-full max-w-xs border {error ? 'border border-red-600' : ''} {$$props.class}" on:input={binder} />
     </div>
 </div>
