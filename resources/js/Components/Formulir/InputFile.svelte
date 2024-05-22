aa0bdf4ed848f3a4140cc6cdc5894fc7d00837da
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-05-22 15:00:40 +0700

    update InputFile style

--- resources/js/Components/Formulir/InputFile.svelte
+++ resources/js/Components/Formulir/InputFile.svelte
@@ -4,4 +4,4 @@
     export let error = ''
 </script>
 
-<Input type="file" binder={(e) => (value = e.target.files[0])} {error}><slot>Input File</slot></Input>
+<Input type="file" binder={(e) => (value = e.target.files[0])} {error} class="file-input"><slot>Input File</slot></Input>
