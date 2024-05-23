b9f0c4b37592414c02de62cdeb2a7b26bf6ef0d8
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-05-23 20:17:22 +0700

    add flash message to layout

--- resources/js/Components/Layout.svelte
+++ resources/js/Components/Layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import Navbar from './Layout/Navbar.svelte'
     import Drawer from './Layout/Drawer.svelte'
+    import FlashMessages from './Layout/FlashMessages.svelte'
 
 </script>
 
 <Navbar />
 <Drawer>
+    <FlashMessages />
     <slot />
 </Drawer>
