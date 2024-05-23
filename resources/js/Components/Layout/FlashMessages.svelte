eaea65192b03974696c86b2dd999f9e5938baae0
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-05-23 20:15:58 +0700

    add flash message component

---
+++ resources/js/Components/Layout/FlashMessages.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { page } from "@inertiajs/svelte";
+    import { onMount } from 'svelte'
+    
+    let errors = $page.props.errors
+    let flash = $page.props.message
+
+    onMount(() => {
+        setTimeout(() => {
+            errors = {}
+            flash = null
+        }, 3000)
+    })
+
+</script>
+
+{#if flash}
+    <div class="alert alert-info">{flash}</div>
+{/if}
+{#each Object.entries(errors) as [k, v]}
+    <div class="alert alert-error">{k}: {v}</div>
+{/each}
